67da13e5105aeacf58df263edfd4d4c20ee378f2
Alex <alexmihgel@gmail.com>
2022-05-18 20:25:09 +0300

    Initial Commit

---
+++ usov_alex_Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usov_alex_Contacts
+//
+//  Created by Алексей Попроцкий on 18.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
