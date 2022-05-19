415e391ea13f78429afd0cf100713fff4788c39b
Alex <alexmihgel@gmail.com>
2022-05-19 21:23:24 +0300

    Комментарии по UserDefault
     добавлены разъяснение по работе UserDefault

--- usov_alex_Contacts/Model/Contact.swift
+++ usov_alex_Contacts/Model/Contact.swift
@@ -27,7 +27,7 @@ struct Contact: ContactProtocol {
 class ContactStorage: ContactStorageProtocol {
     
     private var storageUserDefault = UserDefaults.standard // ссылка на хранилище
-    private var KeyStorageUserDefault = "contacts" // ключ по которому будет происходить сохранение
+    private var keyStorageUserDefault = "contacts" // ключ по которому будет происходить сохранение
     
     private enum ContactKey: String {
         case title
@@ -38,25 +38,25 @@ class ContactStorage: ContactStorageProtocol {
         var arrayForStorage: [[String : String]] = []
         contacts.forEach { contacts in
             var newElementForStorage: [String : String] = [:]
-            newElementForStorage[ContactKey.title.rawValue] = contacts.title
+            newElementForStorage[ContactKey.phone.rawValue] = contacts.phone
+            newElementForStorage[ContactKey.title.rawValue] = contacts.title //c помощью rawValue получаем title как String
             //print("c rawValue \(ContactKey.title.rawValue)")
             //print("без rawValue \(ContactKey.title)")
-            newElementForStorage[ContactKey.phone.rawValue] = contacts.phone
-            //print("save newElementForStorage title \(newElementForStorage)") // newElementForStorage имеет вид = ["title": "Den", "phone": "+79996385190"]
-
+            //print("save newElementForStorage title \(newElementForStorage)")
+            // newElementForStorage имеет вид = ["title": "Den", "phone": "+79996385190"]
             arrayForStorage.append(newElementForStorage)
-            //print("save arrayForStorage \(arrayForStorage)")
+            print("save arrayForStorage \(arrayForStorage)")
 
         }
-        storageUserDefault.set(arrayForStorage, forKey: KeyStorageUserDefault)
+        storageUserDefault.set(arrayForStorage, forKey: keyStorageUserDefault)
     }
     
     func load() -> [ContactProtocol] {
         var resultContacts: [ContactProtocol] = []
-        let contactFromStorage = storageUserDefault.array(forKey: KeyStorageUserDefault)
+        let contactFromStorage = storageUserDefault.array(forKey: keyStorageUserDefault)
                                 as? [[String : String]] ?? []
         
-        for contact in contactFromStorage {
+        for contact in contactFromStorage { //contact это словарь, см его тип. Для каждого словаря contact в массиве contactFromStorage, выполнить.
             guard let title = contact[ContactKey.title.rawValue],
                   let phone = contact[ContactKey.phone.rawValue]
             else {
